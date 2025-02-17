cdfb4f4ee7c05e7cb54f0bc019c164a89f901617
wjwj910 <wjwj910@gmail.com>
2025-02-17 19:05:00 +0900

    frontend)카카오 CDN 도메인 와일드 카드 처리

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -6,11 +6,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "http",
-        hostname: "img1.kakaocdn.net",
+        hostname: "*.kakaocdn.net",
       },
       {
         protocol: "https",
-        hostname: "img1.kakaocdn.net",
+        hostname: "*.kakaocdn.net",
       },
       {
         protocol: "https",
