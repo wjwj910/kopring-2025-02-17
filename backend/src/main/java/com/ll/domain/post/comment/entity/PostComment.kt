0dbac1058e0b68f1641e4fc07d9ee90884841ea4
wjwj910 <wjwj910@gmail.com>
2025-02-18 11:10:11 +0900

    PostComment, 코틀린 변환

--- backend/src/main/java/com/ll/domain/post/comment/entity/PostComment.kt
+++ backend/src/main/java/com/ll/domain/post/comment/entity/PostComment.kt
@@ -1,54 +1,50 @@
 package com.ll.domain.post.comment.entity;
 
-import com.ll.domain.member.member.entity.Member;
-import com.ll.domain.post.post.entity.Post;
-import com.ll.global.exceptions.ServiceException;
-import com.ll.global.jpa.entity.BaseTime;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ManyToOne;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.ll.domain.member.member.entity.Member
+import com.ll.domain.post.post.entity.Post
+import com.ll.global.exceptions.ServiceException
+import com.ll.global.jpa.entity.BaseTime
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.ManyToOne
 
 @Entity
-@Getter
-@NoArgsConstructor
-public class PostComment extends BaseTime {
+class PostComment : BaseTime {
     @ManyToOne(fetch = FetchType.LAZY)
-    private Post post;
+    lateinit var post: Post
 
     @ManyToOne(fetch = FetchType.LAZY)
-    private Member author;
+    lateinit var author: Member
 
     @Column(columnDefinition = "TEXT")
-    private String content;
+    lateinit var content: String
 
-    public PostComment(Post post, Member author, String content) {
+    constructor(post: Post, author: Member, content: String) {
         this.post = post;
         this.author = author;
         this.content = content;
     }
 
-    public void modify(String content) {
-        this.content = content;
+    fun modify(content: String) {
+        this.content = content
     }
 
-    public void checkActorCanModify(Member actor) {
-        if (actor == null) throw new ServiceException("401-1", "로그인 후 이용해주세요.");
+    fun checkActorCanModify(actor: Member?) {
+        if (actor == null) throw ServiceException("401-1", "로그인 후 이용해주세요.")
 
-        if (actor.equals(author)) return;
+        if (actor == author) return
 
-        throw new ServiceException("403-2", "작성자만 댓글을 수정할 수 있습니다.");
+        throw ServiceException("403-2", "작성자만 댓글을 수정할 수 있습니다.")
     }
 
-    public void checkActorCanDelete(Member actor) {
-        if (actor == null) throw new ServiceException("401-1", "로그인 후 이용해주세요.");
+    fun checkActorCanDelete(actor: Member?) {
+        if (actor == null) throw ServiceException("401-1", "로그인 후 이용해주세요.")
 
-        if (actor.isAdmin()) return;
+        if (actor.isAdmin) return
 
-        if (actor.equals(author)) return;
+        if (actor == author) return
 
-        throw new ServiceException("403-2", "작성자만 댓글을 삭제할 수 있습니다.");
+        throw ServiceException("403-2", "작성자만 댓글을 삭제할 수 있습니다.")
     }
 }
