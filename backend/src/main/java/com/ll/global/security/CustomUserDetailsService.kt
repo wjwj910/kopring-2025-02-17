47465b7c6fe30215bebd9093973f76a51e0963b1
wjwj910 <wjwj910@gmail.com>
2025-02-19 14:32:18 +0900

    CustomUserDetailsService, 코틀린 변환

--- backend/src/main/java/com/ll/global/security/CustomUserDetailsService.kt
+++ backend/src/main/java/com/ll/global/security/CustomUserDetailsService.kt
@@ -1,29 +1,26 @@
 package com.ll.global.security;
 
-import com.ll.domain.member.member.entity.Member;
-import com.ll.domain.member.member.repository.MemberRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import com.ll.domain.member.member.repository.MemberRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
 
 @Service
-@RequiredArgsConstructor
-public class CustomUserDetailsService implements UserDetailsService {
-    private final MemberRepository memberRepository;
+class CustomUserDetailsService(
+    private val memberRepository: MemberRepository
+) : UserDetailsService {
+    override fun loadUserByUsername(username: String): UserDetails {
+        val member = memberRepository.findByUsername(username).orElseThrow {
+            UsernameNotFoundException("사용자를 찾을 수 없습니다.")
+        }
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member member = memberRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
-
-        return new SecurityUser(
-                member.getId(),
-                member.getUsername(),
-                member.getPassword(),
-                member.getNickname(),
-                member.getAuthorities()
+        return SecurityUser(
+            member.id,
+            member.username,
+            member.password,
+            member.nickname,
+            member.authorities
         );
     }
 }
