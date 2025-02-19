b7284f42a680e2527730a095554ce0c8b3cb56f2
wjwj910 <wjwj910@gmail.com>
2025-02-19 14:33:24 +0900

    CustomOAuth2AuthenticationSuccessHandler, 코틀린 변환

--- backend/src/main/java/com/ll/global/security/CustomOAuth2AuthenticationSuccessHandler.kt
+++ backend/src/main/java/com/ll/global/security/CustomOAuth2AuthenticationSuccessHandler.kt
@@ -1,30 +1,29 @@
 package com.ll.global.security;
 
-import com.ll.domain.member.member.entity.Member;
-import com.ll.domain.member.member.service.MemberService;
-import com.ll.global.rq.Rq;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.stereotype.Component;
+import com.ll.domain.member.member.service.MemberService
+import com.ll.global.rq.Rq
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler
+import org.springframework.stereotype.Component
 
 @Component
-@RequiredArgsConstructor
-public class CustomOAuth2AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
-    private final MemberService memberService;
-    private final Rq rq;
+class CustomOAuth2AuthenticationSuccessHandler(
+    private val memberService: MemberService,
+    private val rq: Rq
+) : SavedRequestAwareAuthenticationSuccessHandler() {
 
-    @SneakyThrows
-    @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        Member actor = memberService.findById(rq.getActor().getId()).get();
-        rq.makeAuthCookies(actor);
+    override fun onAuthenticationSuccess(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        authentication: Authentication
+    ) {
+        val actor = memberService.findById(rq.actor!!.id).get()
+        rq.makeAuthCookies(actor)
 
-        String redirectUrl = request.getParameter("state");
+        val redirectUrl = request.getParameter("state")
 
-        response.sendRedirect(redirectUrl);
+        response.sendRedirect(redirectUrl)
     }
 }
