d49e5a8815267f5f2204942ef77e9171bc08e1f1
wjwj910 <wjwj910@gmail.com>
2025-02-19 11:47:16 +0900

    AppConfig, 코틀린 변환

--- backend/src/main/java/com/ll/global/app/AppConfig.kt
+++ backend/src/main/java/com/ll/global/app/AppConfig.kt
@@ -1,122 +1,112 @@
-package com.ll.global.app;
+package com.ll.global.app
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Getter;
-import lombok.SneakyThrows;
-import org.apache.tika.Tika;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.core.io.ClassPathResource;
+import com.fasterxml.jackson.databind.ObjectMapper
+import lombok.SneakyThrows
+import org.apache.tika.Tika
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
+import org.springframework.core.io.ClassPathResource
 
 @Configuration
-public class AppConfig {
-    private static Environment environment;
+class AppConfig {
+    companion object {
+        private lateinit var environment: Environment
+        private lateinit var objectMapper: ObjectMapper
+        private lateinit var tika: Tika
+        private lateinit var siteFrontUrl: String
+        private lateinit var siteBackUrl: String
+        private lateinit var genFileDirPath: String
+        private lateinit var springServletMultipartMaxFileSize: String
+        private lateinit var springServletMultipartMaxRequestSize: String
+        private var resourcesSampleDirPath: String? = null
 
-    @Autowired
-    public void setEnvironment(Environment environment) {
-        AppConfig.environment = environment;
-    }
+        @JvmStatic
+        fun isProd(): Boolean = environment.matchesProfiles("prod")
 
-    public static boolean isProd() {
-        return environment.matchesProfiles("prod");
-    }
+        @JvmStatic
+        fun isDev(): Boolean = environment.matchesProfiles("dev")
 
-    public static boolean isDev() {
-        return environment.matchesProfiles("dev");
-    }
+        @JvmStatic
+        fun isTest(): Boolean = environment.matchesProfiles("test")
 
-    public static boolean isTest() {
-        return environment.matchesProfiles("test");
-    }
+        @JvmStatic
+        fun isNotProd(): Boolean = !isProd()
 
-    public static boolean isNotProd() {
-        return !isProd();
-    }
+        @JvmStatic
+        fun getObjectMapper(): ObjectMapper = objectMapper
 
-    private static ObjectMapper objectMapper;
+        @JvmStatic
+        fun getTika(): Tika = tika
 
-    public static ObjectMapper getObjectMapper() {
-        return objectMapper;
-    }
+        @JvmStatic
+        fun getSiteFrontUrl(): String = siteFrontUrl
 
-    @Autowired
-    public void setObjectMapper(ObjectMapper objectMapper) {
-        AppConfig.objectMapper = objectMapper;
-    }
+        @JvmStatic
+        fun getSiteBackUrl(): String = siteBackUrl
 
-    private static Tika tika;
+        @JvmStatic
+        fun getGenFileDirPath(): String = genFileDirPath
 
-    public static Tika getTika() {
-        return tika;
-    }
+        @JvmStatic
+        fun getSpringServletMultipartMaxFileSize(): String = springServletMultipartMaxFileSize
 
-    @Autowired
-    public void setTika(Tika tika) {
-        AppConfig.tika = tika;
-    }
+        @JvmStatic
+        fun getSpringServletMultipartMaxRequestSize(): String = springServletMultipartMaxRequestSize
 
-    @Getter
-    private static String siteFrontUrl;
+        @JvmStatic
+        fun getTempDirPath(): String = System.getProperty("java.io.tmpdir")
 
-    @Value("${custom.site.frontUrl}")
-    public void setSiteFrontUrl(String siteFrontUrl) {
-        AppConfig.siteFrontUrl = siteFrontUrl;
-    }
+        @JvmStatic
+        @SneakyThrows
+        fun getResourcesSampleDirPath(): String {
+            if (resourcesSampleDirPath == null) {
+                val resource = ClassPathResource("sample")
+                resourcesSampleDirPath = resource.file.absolutePath
+            }
 
-    private static String siteBackUrl;
-
-    public static String getSiteBackUrl() {
-        return siteBackUrl;
-    }
-
-    @Value("${custom.site.backUrl}")
-    public void setSiteBackUrl(String siteBackUrl) {
-        AppConfig.siteBackUrl = siteBackUrl;
-    }
-
-    public static String genFileDirPath;
-
-    public static String getGenFileDirPath() {
-        return genFileDirPath;
-    }
-
-    @Value("${custom.genFile.dirPath}")
-    public void setGenFileDirPath(String genFileDirPath) {
-        this.genFileDirPath = genFileDirPath;
-    }
-
-    @Getter
-    private static String springServletMultipartMaxFileSize;
-
-    @Value("${spring.servlet.multipart.max-file-size}")
-    public void setSpringServletMultipartMaxFileSize(String springServletMultipartMaxFileSize) {
-        this.springServletMultipartMaxFileSize = springServletMultipartMaxFileSize;
-    }
-
-    @Getter
-    private static String springServletMultipartMaxRequestSize;
-
-    @Value("${spring.servlet.multipart.max-request-size}")
-    public void setSpringServletMultipartMaxRequestSize(String springServletMultipartMaxRequestSize) {
-        this.springServletMultipartMaxRequestSize = springServletMultipartMaxRequestSize;
-    }
-
-    public static String getTempDirPath() {
-        return System.getProperty("java.io.tmpdir");
-    }
-
-    private static String resourcesSampleDirPath;
-
-    @SneakyThrows
-    public static String getResourcesSampleDirPath() {
-        if (resourcesSampleDirPath == null) {
-            ClassPathResource resource = new ClassPathResource("sample");
-
-            resourcesSampleDirPath = resource.getFile().getAbsolutePath();
+            return resourcesSampleDirPath!!
         }
+    }
 
-        return resourcesSampleDirPath;
+    @Autowired
+    fun setEnvironment(environment: Environment) {
+        Companion.environment = environment
+    }
+
+    @Autowired
+    fun setObjectMapper(objectMapper: ObjectMapper) {
+        Companion.objectMapper = objectMapper
+    }
+
+    @Autowired
+    fun setTika(tika: Tika) {
+        Companion.tika = tika
+    }
+
+    @Value("\${custom.site.frontUrl}")
+    fun setSiteFrontUrl(siteFrontUrl: String) {
+        Companion.siteFrontUrl = siteFrontUrl
+    }
+
+    @Value("\${custom.site.backUrl}")
+    fun setSiteBackUrl(siteBackUrl: String) {
+        Companion.siteBackUrl = siteBackUrl
+    }
+
+    @Value("\${custom.genFile.dirPath}")
+    fun setGenFileDirPath(genFileDirPath: String) {
+        Companion.genFileDirPath = genFileDirPath
+    }
+
+    @Value("\${spring.servlet.multipart.max-file-size}")
+    fun setSpringServletMultipartMaxFileSize(springServletMultipartMaxFileSize: String) {
+        Companion.springServletMultipartMaxFileSize = springServletMultipartMaxFileSize
+    }
+
+    @Value("\${spring.servlet.multipart.max-request-size}")
+    fun setSpringServletMultipartMaxRequestSize(springServletMultipartMaxRequestSize: String) {
+        Companion.springServletMultipartMaxRequestSize = springServletMultipartMaxRequestSize
     }
 }
