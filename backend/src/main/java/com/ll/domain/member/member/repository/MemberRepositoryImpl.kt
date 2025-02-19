d62bbc6ba7257c2b7d8ea8dd4f44c1d78544de66
wjwj910 <wjwj910@gmail.com>
2025-02-19 16:04:39 +0900

    MemberRepositoryImpl, 코틀린 변환

--- backend/src/main/java/com/ll/domain/member/member/repository/MemberRepositoryImpl.kt
+++ backend/src/main/java/com/ll/domain/member/member/repository/MemberRepositoryImpl.kt
@@ -1,72 +1,76 @@
-package com.ll.domain.member.member.repository;
+package com.ll.domain.member.member.repository
 
-import com.ll.domain.member.member.entity.Member;
-import com.ll.standard.search.MemberSearchKeywordTypeV1;
-import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.Order;
-import com.querydsl.core.types.OrderSpecifier;
-import com.querydsl.core.types.dsl.PathBuilder;
-import com.querydsl.jpa.impl.JPAQuery;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.support.PageableExecutionUtils;
+import com.ll.domain.member.member.entity.Member
+import com.ll.domain.member.member.entity.QMember
+import com.ll.standard.search.MemberSearchKeywordTypeV1
+import com.querydsl.core.BooleanBuilder
+import com.querydsl.core.types.Expression
+import com.querydsl.core.types.Order
+import com.querydsl.core.types.OrderSpecifier
+import com.querydsl.core.types.dsl.PathBuilder
+import com.querydsl.jpa.impl.JPAQuery
+import com.querydsl.jpa.impl.JPAQueryFactory
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.support.PageableExecutionUtils
 
-import static com.ll.domain.member.member.entity.QMember.member;
+class MemberRepositoryImpl(
+    private val jpaQueryFactory: JPAQueryFactory
+) : MemberRepositoryCustom {
+    override fun findByKw(kwType: MemberSearchKeywordTypeV1, kw: String, pageable: Pageable): Page<Member> {
+        val builder = BooleanBuilder()
 
-@RequiredArgsConstructor
-public class MemberRepositoryImpl implements MemberRepositoryCustom {
-    private final JPAQueryFactory jpaQueryFactory;
-
-    @Override
-    public Page<Member> findByKw(MemberSearchKeywordTypeV1 kwType, String kw, Pageable pageable) {
-        BooleanBuilder builder = new BooleanBuilder();
-
-        if (kw != null && !kw.isBlank()) {
-            applyKeywordFilter(kwType, kw, builder);
+        if (kw.isNotBlank()) {
+            applyKeywordFilter(kwType, kw, builder)
         }
 
-        JPAQuery<Member> membersQuery = createMembersQuery(builder);
-        applySorting(pageable, membersQuery);
+        val membersQuery = createMembersQuery(builder)
+        applySorting(pageable, membersQuery)
 
-        membersQuery.offset(pageable.getOffset()).limit(pageable.getPageSize());
+        membersQuery.offset(pageable.offset).limit(pageable.pageSize.toLong())
 
-        JPAQuery<Long> totalQuery = createTotalQuery(builder);
+        val totalQuery = createTotalQuery(builder)
 
-        return PageableExecutionUtils.getPage(membersQuery.fetch(), pageable, totalQuery::fetchOne);
+        return PageableExecutionUtils.getPage(
+            membersQuery.fetch(), pageable
+        ) { totalQuery.fetchOne()!! }
     }
 
-    private void applyKeywordFilter(MemberSearchKeywordTypeV1 kwType, String kw, BooleanBuilder builder) {
-        switch (kwType) {
-            case kwType.username -> builder.and(member.username.containsIgnoreCase(kw));
-            case kwType.nickname -> builder.and(member.nickname.containsIgnoreCase(kw));
-            default -> builder.and(
-                    member.username.containsIgnoreCase(kw)
-                            .or(member.nickname.containsIgnoreCase(kw))
-            );
+    private fun applyKeywordFilter(kwType: MemberSearchKeywordTypeV1, kw: String, builder: BooleanBuilder) {
+        when (kwType) {
+            MemberSearchKeywordTypeV1.username -> builder.and(QMember.member.username.containsIgnoreCase(kw))
+            MemberSearchKeywordTypeV1.nickname -> builder.and(QMember.member.nickname.containsIgnoreCase(kw))
+            else -> builder.and(
+                QMember.member.username.containsIgnoreCase(kw)
+                    .or(QMember.member.nickname.containsIgnoreCase(kw))
+            )
         }
     }
 
-    private JPAQuery<Member> createMembersQuery(BooleanBuilder builder) {
+    private fun createMembersQuery(builder: BooleanBuilder): JPAQuery<Member> {
         return jpaQueryFactory
-                .select(member)
-                .from(member)
-                .where(builder);
+            .select(QMember.member)
+            .from(QMember.member)
+            .where(builder)
     }
 
-    private void applySorting(Pageable pageable, JPAQuery<Member> membersQuery) {
-        for (Sort.Order o : pageable.getSort()) {
-            PathBuilder pathBuilder = new PathBuilder(member.getType(), member.getMetadata());
-            membersQuery.orderBy(new OrderSpecifier(o.isAscending() ? Order.ASC : Order.DESC, pathBuilder.get(o.getProperty())));
+    private fun applySorting(pageable: Pageable, membersQuery: JPAQuery<Member>) {
+        for (o in pageable.sort) {
+            val pathBuilder: PathBuilder<*> = PathBuilder<Any?>(QMember.member.type, QMember.member.metadata)
+
+            membersQuery.orderBy(
+                OrderSpecifier(
+                    if (o.isAscending) Order.ASC else Order.DESC,
+                    pathBuilder[o.property] as Expression<Comparable<*>>
+                )
+            )
         }
     }
 
-    private JPAQuery<Long> createTotalQuery(BooleanBuilder builder) {
+    private fun createTotalQuery(builder: BooleanBuilder): JPAQuery<Long> {
         return jpaQueryFactory
-                .select(member.count())
-                .from(member)
-                .where(builder);
+            .select(QMember.member.count())
+            .from(QMember.member)
+            .where(builder)
     }
 }
