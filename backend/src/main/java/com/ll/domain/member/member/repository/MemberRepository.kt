4af9b9f94b79fb1c08ab5f43c66d2ffa7723d0dc
wjwj910 <wjwj910@gmail.com>
2025-02-19 16:03:48 +0900

    MemberRepository, 코틀린 변환

--- backend/src/main/java/com/ll/domain/member/member/repository/MemberRepository.kt
+++ backend/src/main/java/com/ll/domain/member/member/repository/MemberRepository.kt
@@ -1,12 +1,12 @@
-package com.ll.domain.member.member.repository;
+package com.ll.domain.member.member.repository
 
-import com.ll.domain.member.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.ll.domain.member.member.entity.Member
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
-import java.util.Optional;
+interface MemberRepository : JpaRepository<Member, Long>,
+    MemberRepositoryCustom {
+    fun findByUsername(username: String): Optional<Member>
 
-public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
-    Optional<Member> findByUsername(String username);
-
-    Optional<Member> findByApiKey(String apiKey);
+    fun findByApiKey(apiKey: String): Optional<Member>
 }
--- backend/src/main/java/com/ll/domain/member/member/repository/MemberRepositoryCustom.kt
+++ backend/src/main/java/com/ll/domain/member/member/repository/MemberRepositoryCustom.kt
@@ -1,10 +1,10 @@
-package com.ll.domain.member.member.repository;
+package com.ll.domain.member.member.repository
 
-import com.ll.domain.member.member.entity.Member;
-import com.ll.standard.search.MemberSearchKeywordTypeV1;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import com.ll.domain.member.member.entity.Member
+import com.ll.standard.search.MemberSearchKeywordTypeV1
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 
-public interface MemberRepositoryCustom {
-    Page<Member> findByKw(MemberSearchKeywordTypeV1 kwType, String kw, Pageable pageable);
+interface MemberRepositoryCustom {
+    fun findByKw(kwType: MemberSearchKeywordTypeV1, kw: String, pageable: Pageable): Page<Member>
 }
