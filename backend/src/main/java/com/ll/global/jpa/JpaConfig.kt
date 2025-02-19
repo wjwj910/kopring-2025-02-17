5e75bdc007c1f162f191d66e92a45ad05049b7c7
wjwj910 <wjwj910@gmail.com>
2025-02-19 14:41:22 +0900

    JpaConfig/SpringDocConfig, 코틀린 변환

--- backend/src/main/java/com/ll/global/jpa/JpaConfig.kt
+++ backend/src/main/java/com/ll/global/jpa/JpaConfig.kt
@@ -1,19 +1,16 @@
-package com.ll.global.jpa;
+package com.ll.global.jpa
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import com.querydsl.jpa.impl.JPAQueryFactory
+import jakarta.persistence.EntityManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @Configuration
-public class JpaConfig {
-
-    @PersistenceContext
-    private EntityManager entityManager;
-
+class JpaConfig(
+    private val entityManager: EntityManager
+) {
     @Bean
-    public JPAQueryFactory jpaQueryFactory() {
-        return new JPAQueryFactory(entityManager);
+    fun jpaQueryFactory(): JPAQueryFactory {
+        return JPAQueryFactory(entityManager)
     }
 }
--- backend/src/main/java/com/ll/global/springDoc/SpringDocConfig.kt
+++ backend/src/main/java/com/ll/global/springDoc/SpringDocConfig.kt
@@ -1,35 +1,30 @@
-package com.ll.global.springDoc;
+package com.ll.global.springDoc
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.security.SecurityScheme
+import org.springdoc.core.models.GroupedOpenApi
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "API 서버", version = "v1"))
-@SecurityScheme(
-        name = "bearerAuth",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
-)
-public class SpringDocConfig {
+@OpenAPIDefinition(info = Info(title = "API 서버", version = "v1"))
+@SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, bearerFormat = "JWT", scheme = "bearer")
+class SpringDocConfig {
     @Bean
-    public GroupedOpenApi groupApiV1() {
+    fun groupApiV1(): GroupedOpenApi {
         return GroupedOpenApi.builder()
-                .group("apiV1")
-                .pathsToMatch("/api/v1/**")
-                .build();
+            .group("apiV1")
+            .pathsToMatch("/api/v1/**")
+            .build()
     }
 
     @Bean
-    public GroupedOpenApi groupController() {
+    fun groupController(): GroupedOpenApi {
         return GroupedOpenApi.builder()
-                .group("controller")
-                .pathsToExclude("/api/**")
-                .build();
+            .group("controller")
+            .pathsToExclude("/api/**")
+            .build()
     }
 }
