20f0925e7f2cdc234fbc3cfe68a998ea0c85bdff
wjwj910 <wjwj910@gmail.com>
2025-02-19 15:07:45 +0900

    DevInitData, 코틀린 변환

--- backend/src/main/java/com/ll/global/initData/DevInitData.kt
+++ backend/src/main/java/com/ll/global/initData/DevInitData.kt
@@ -1,33 +1,35 @@
-package com.ll.global.initData;
+package com.ll.global.initData
 
-import com.ll.domain.member.member.service.MemberService;
-import com.ll.domain.post.post.service.PostService;
-import com.ll.standard.util.Ut;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.context.annotation.Profile;
+import com.ll.domain.member.member.service.MemberService
+import com.ll.domain.post.post.service.PostService
+import com.ll.standard.util.Ut.cmd.runAsync
+import com.ll.standard.util.Ut.file.downloadByHttp
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.ApplicationRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Lazy
+import org.springframework.context.annotation.Profile
 
 @Profile("dev")
 @Configuration
-@RequiredArgsConstructor
-public class DevInitData {
-    private final MemberService memberService;
-    private final PostService postService;
+class DevInitData(
+    private val memberService: MemberService,
+    private val postService: PostService
+) {
     @Autowired
     @Lazy
-    private DevInitData self;
+    private lateinit var self: DevInitData
 
     @Bean
-    public ApplicationRunner devInitDataApplicationRunner() {
-        return args -> {
-            Ut.file.downloadByHttp("http://localhost:8080/v3/api-docs/apiV1", ".", false);
+    fun devInitDataApplicationRunner(): ApplicationRunner {
+        return ApplicationRunner {
+            downloadByHttp("http://localhost:8080/v3/api-docs/apiV1", ".", false)
 
-            String cmd = "yes | npx --package typescript --package openapi-typescript openapi-typescript apiV1.json -o ../frontend/src/lib/backend/apiV1/schema.d.ts";
-            Ut.cmd.runAsync(cmd);
-        };
+            val cmd =
+                "yes | npx --package typescript --package openapi-typescript openapi-typescript apiV1.json -o ../frontend/src/lib/backend/apiV1/schema.d.ts"
+
+            runAsync(cmd)
+        }
     }
 }
