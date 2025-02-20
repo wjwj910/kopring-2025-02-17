af3d317efa27ba8f8ff2fe07c675dad247818c11
wjwj910 <wjwj910@gmail.com>
2025-02-20 09:29:10 +0900

    ApiV1PostCommentController, 코틀린 변환

--- backend/src/main/java/com/ll/domain/post/comment/controller/ApiV1PostCommentController.kt
+++ backend/src/main/java/com/ll/domain/post/comment/controller/ApiV1PostCommentController.kt
@@ -1,150 +1,155 @@
-package com.ll.domain.post.comment.controller;
+package com.ll.domain.post.comment.controller
 
-import com.ll.domain.member.member.entity.Member;
-import com.ll.domain.post.comment.dto.PostCommentDto;
-import com.ll.domain.post.comment.entity.PostComment;
-import com.ll.domain.post.post.entity.Post;
-import com.ll.domain.post.post.service.PostService;
-import com.ll.global.exceptions.ServiceException;
-import com.ll.global.rq.Rq;
-import com.ll.global.rsData.RsData;
-import com.ll.standard.base.Empty;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import com.ll.domain.post.comment.dto.PostCommentDto
+import com.ll.domain.post.post.service.PostService
+import com.ll.global.exceptions.ServiceException
+import com.ll.global.rq.Rq
+import com.ll.global.rsData.RsData
+import com.ll.standard.base.Empty
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/posts/{postId}/comments")
-@RequiredArgsConstructor
 @Tag(name = "ApiV1PostCommentController", description = "API 댓글 컨트롤러")
 @SecurityRequirement(name = "bearerAuth")
-public class ApiV1PostCommentController {
-    private final PostService postService;
-    private final Rq rq;
-
-
+class ApiV1PostCommentController(
+    private val postService: PostService,
+    private val rq: Rq
+) {
     @GetMapping
     @Transactional(readOnly = true)
     @Operation(summary = "다건조회")
-    public List<PostCommentDto> items(
-            @PathVariable long postId
-    ) {
-        Post post = postService.findById(postId).orElseThrow(
-                () -> new ServiceException("404-1", "%d번 글은 존재하지 않습니다.".formatted(postId))
-        );
+    fun items(
+        @PathVariable postId: Long
+    ): List<PostCommentDto> {
+        val post = postService.findById(postId).orElseThrow {
+            ServiceException(
+                "404-1",
+                "%d번 글은 존재하지 않습니다.".formatted(postId)
+            )
+        }
 
         return post
-                .getComments()
-                .stream()
-                .map(PostCommentDto::new)
-                .toList();
+            .comments
+            .stream()
+            .map { PostCommentDto(it) }
+            .toList()
     }
 
 
     @DeleteMapping("/{id}")
     @Transactional
     @Operation(summary = "삭제")
-    public RsData<Empty> delete(
-            @PathVariable long postId,
-            @PathVariable long id
-    ) {
-        Member actor = rq.getActor();
+    fun delete(
+        @PathVariable postId: Long,
+        @PathVariable id: Long
+    ): RsData<Empty> {
+        val actor = rq.actor!!
 
-        Post post = postService.findById(postId).orElseThrow(
-                () -> new ServiceException("404-1", "%d번 글은 존재하지 않습니다.".formatted(postId))
-        );
+        val post = postService.findById(postId).orElseThrow {
+            ServiceException(
+                "404-1",
+                "${postId}번 글은 존재하지 않습니다."
+            )
+        }
 
-        PostComment postComment = post.getCommentById(id).orElseThrow(
-                () -> new ServiceException("404-2", "%d번 댓글은 존재하지 않습니다.".formatted(id))
-        );
+        val postComment = post.getCommentById(id).orElseThrow {
+            ServiceException(
+                "404-2",
+                "${id}번 댓글은 존재하지 않습니다."
+            )
+        }
 
-        postComment.checkActorCanDelete(actor);
+        postComment.checkActorCanDelete(actor)
 
-        post.removeComment(postComment);
+        post.removeComment(postComment)
 
-        return new RsData<>(
-                "200-1",
-                "%d번 댓글이 삭제되었습니다.".formatted(id)
-        );
+        return RsData(
+            "200-1",
+            "%d번 댓글이 삭제되었습니다.".formatted(id)
+        )
     }
 
 
-    record PostCommentModifyReqBody(
-            @NotBlank
-            @Size(min = 2, max = 100)
-            String content
-    ) {
-    }
+    @JvmRecord
+    data class PostCommentModifyReqBody(
+        @field:NotBlank @field:Size(min = 2, max = 100) val content: String
+    )
 
     @PutMapping("/{id}")
     @Transactional
     @Operation(summary = "수정")
-    public RsData<PostCommentDto> modify(
-            @PathVariable long postId,
-            @PathVariable long id,
-            @RequestBody @Valid PostCommentModifyReqBody reqBody
-    ) {
-        Member actor = rq.getActor();
+    fun modify(
+        @PathVariable postId: Long,
+        @PathVariable id: Long,
+        @RequestBody @Valid reqBody: PostCommentModifyReqBody
+    ): RsData<PostCommentDto> {
+        val actor = rq.actor!!
 
-        Post post = postService.findById(postId).orElseThrow(
-                () -> new ServiceException("404-1", "%d번 글은 존재하지 않습니다.".formatted(postId))
-        );
+        val post = postService.findById(postId).orElseThrow {
+            ServiceException(
+                "404-1",
+                "%d번 글은 존재하지 않습니다.".formatted(postId)
+            )
+        }
 
-        PostComment postComment = post.getCommentById(id).orElseThrow(
-                () -> new ServiceException("404-2", "%d번 댓글은 존재하지 않습니다.".formatted(id))
-        );
+        val postComment = post.getCommentById(id).orElseThrow {
+            ServiceException(
+                "404-2",
+                "${id}번 댓글은 존재하지 않습니다."
+            )
+        }
 
-        postComment.checkActorCanModify(actor);
+        postComment.checkActorCanModify(actor)
 
-        postComment.modify(reqBody.content);
+        postComment.modify(reqBody.content)
 
-        return new RsData<>(
-                "200-1",
-                "%d번 댓글이 수정되었습니다.".formatted(id),
-                new PostCommentDto(postComment)
-        );
+        return RsData(
+            "200-1",
+            "${id}번 댓글이 수정되었습니다.",
+            PostCommentDto(postComment)
+        )
     }
 
 
-    record PostCommentWriteReqBody(
-            @NotBlank
-            @Size(min = 2, max = 100)
-            String content
-    ) {
-    }
+    data class PostCommentWriteReqBody(
+        @field:NotBlank @field:Size(min = 2, max = 100) val content: String
+    )
 
     @PostMapping
     @Transactional
     @Operation(summary = "작성")
-    public RsData<PostCommentDto> write(
-            @PathVariable long postId,
-            @RequestBody @Valid PostCommentWriteReqBody reqBody
-    ) {
-        Member actor = rq.getActor();
+    fun write(
+        @PathVariable postId: Long,
+        @RequestBody @Valid reqBody: PostCommentWriteReqBody
+    ): RsData<PostCommentDto> {
+        val actor = rq.actor!!
 
-        Post post = postService.findById(postId).orElseThrow(
-                () -> new ServiceException("404-1", "%d번 글은 존재하지 않습니다.".formatted(postId))
-        );
+        val post = postService.findById(postId).orElseThrow {
+            ServiceException(
+                "404-1",
+                "${postId}번 글은 존재하지 않습니다."
+            )
+        }
 
-        PostComment postComment = post.addComment(
-                actor,
-                reqBody.content
-        );
+        val postComment = post.addComment(
+            actor,
+            reqBody.content
+        )
 
-        postService.flush();
+        postService.flush()
 
-        return new RsData<>(
-                "201-1",
-                "%d번 댓글이 생성되었습니다.".formatted(postComment.getId()),
-                new PostCommentDto(postComment)
-        );
+        return RsData(
+            "201-1",
+            "%d번 댓글이 생성되었습니다.".formatted(postComment.id),
+            PostCommentDto(postComment)
+        )
     }
 }
