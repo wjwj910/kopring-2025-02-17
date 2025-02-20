bc684df492bd50bd9901f7a1a080e43000959c20
wjwj910 <wjwj910@gmail.com>
2025-02-20 09:30:37 +0900

    PostGenFileController, 코틀린 변환

--- backend/src/main/java/com/ll/domain/post/genFile/controller/PostGenFileController.kt
+++ backend/src/main/java/com/ll/domain/post/genFile/controller/PostGenFileController.kt
@@ -1,60 +1,54 @@
-package com.ll.domain.post.genFile.controller;
+package com.ll.domain.post.genFile.controller
 
-import com.ll.domain.post.genFile.entity.PostGenFile;
-import com.ll.domain.post.post.entity.Post;
-import com.ll.domain.post.post.service.PostService;
-import com.ll.standard.util.Ut;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import com.ll.domain.post.post.service.PostService
+import com.ll.standard.util.Ut.url.encode
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.core.io.InputStreamResource
+import org.springframework.core.io.Resource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import java.io.FileInputStream
 
 @Controller
 @RequestMapping("/post/genFile")
-@RequiredArgsConstructor
 @Tag(name = "PostGenFileController", description = "파일 다운로드 등 다양한 기능 제공")
-public class PostGenFileController {
-    private final PostService postService;
-
+class PostGenFileController(
+    private val postService: PostService
+) {
     @GetMapping("/download/{postId}/{fileName}")
     @Operation(summary = "파일 다운로드")
-    public ResponseEntity<Resource> download(
-            @PathVariable long postId,
-            @PathVariable String fileName,
-            HttpServletRequest request
-    ) throws FileNotFoundException {
-        Post post = postService.findById(postId).get();
+    fun download(
+        @PathVariable postId: Long,
+        @PathVariable fileName: String,
+        request: HttpServletRequest
+    ): ResponseEntity<Resource> {
+        val post = postService.findById(postId).get()
 
-        PostGenFile genFile = post.getGenFiles()
-                .stream()
-                .filter(f -> f.getFileName().equals(fileName))
-                .findFirst()
-                .get();
+        val genFile = post.genFiles
+            .stream()
+            .filter { it.fileName == fileName }
+            .findFirst()
+            .get()
 
-        String filePath = genFile.getFilePath();
+        val filePath = genFile.filePath
 
-        Resource resource = new InputStreamResource(new FileInputStream(filePath));
+        val resource: Resource = InputStreamResource(FileInputStream(filePath))
 
-        String contentType = request.getServletContext().getMimeType(filePath);
+        var contentType = request.servletContext.getMimeType(filePath)
 
-        if (contentType == null) contentType = "application/octet-stream";
+        if (contentType == null) contentType = "application/octet-stream"
 
-        String downloadFileName = Ut.url.encode(genFile.getOriginalFileName()).replace("%20", " ");
+        val downloadFileName = encode(genFile.originalFileName).replace("%20", " ")
 
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + downloadFileName + "\"")
-                .contentType(MediaType.parseMediaType(contentType)).body(resource);
+            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"$downloadFileName\"")
+            .contentType(MediaType.parseMediaType(contentType)).body(resource)
     }
 }
